04ee9570696a7516c89c173a692daf2bcf38c645
PING HSING WU <pinghsingwu@PINGs-MacBook-Pro.local>
2013-07-30 22:26:47 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Groupkuo::Application.config.secret_key_base = '512bde17baee13904f1bf52567ac94d8f1005576b06fea379de15d3bd8c679c7ea5e0a9dd9cc19726d3615a4e23483cbdfe19ee346ac978c7a764544ddb629a2'
